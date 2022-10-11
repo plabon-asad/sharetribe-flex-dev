048f9678bbd1b6f4060d7630af39df05859a2798
plabon-asad <asad.ku.math@gmail.com>
2022-10-12 04:18:17 +0600

    Logo update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 import config from '../../config';
 import LogoImageMobile from './sneakertime-logo-mobile.png';
-import LogoImage from './sneakertime-logo.png';
+import LogoImage from './gromart-logo.png';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
 
-  if (format === 'desktop') {
-    return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
-  }
+  // if (format === 'desktop') {
+  //   return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
+  // }
 
-  return <img className={className} src={LogoImageMobile} alt={config.siteTitle} {...rest} />;
+  return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
